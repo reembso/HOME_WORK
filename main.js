1f1b65da02956dcf1032acfcf4cb82406c8cf29c
reembso <122732498+reembso@users.noreply.github.com>
2023-04-14 14:09:16 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,114 +1,146 @@
-// TASK 1
-let num1 = prompt("Введите число 1");
-let num2 = prompt("Введите число 2");
-let sum = num1 + num2;
-
-switch (sum) {
-  case 10:
-    alert(10);
-    break;
-  case 20:
-    alert(20);
-    break;
-  default:
-    alert("default");
-    break;
+//TASK1
+function sumFn(a,b) {
+    return a + b
 }
+console.log(sumFn(2,2));
 
 //TASK2
-let month = prompt("Введите название месяца")
-
-switch (month) {
-  case "декабрь":
-  case "январь":
-  case "февраль":
-    alert("Месяц относится к зиме");
-    break;
-  case "март":
-  case "апрель":
-  case "май":
-    alert("Месяц относится к весне");
-    break;
-  case "июнь":
-  case "июль":
-  case "август":
-    alert("Месяц относится к лету");
-    break;
-  case "сентябрь":
-  case "октябрь":
-  case "ноябрь":
-    alert("Месяц относится к осени");
-    break;
-  default:
-    alert("Неверное название месяца");
-    break;
+function changeFn(str) {
+const newStr = str[0].toUpperCase() + str.slice([1])
+console.log(newStr);
 }
+changeFn(`string`)
 
 //TASK3
-let age = prompt("Введите возраст ученика");
-age = parseInt(age);
-
-switch (age) {
-  case 6:
-  case 7:
-    alert("Первоклассник");
-    break;
-  case 8:
-  case 9:
-  case 10:
-    alert("Ученик из начальной школы");
-    break;
-  case 11:
-  case 12:
-  case 13:
-    alert("Ученик из средней школы");
-    break;
-  case 14:
-  case 15:
-  case 16:
-    alert("Ученик из старшей школы");
-    break;
-  default:
-    alert("Не ученик");
-    break;
+function arrayFn(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        result.push(arr[i] * 1);
+    }
+    return result;
 }
+console.log(arrayFn([1,2,3,4,5]));
 
 //TASK4
-const person1 = {
-    age: 17,
-    firstName: "Akusi",
-    lastName: "Zuparova"
-  };
-  
-  console.log("Age:", person1.age);
-  console.log("First Name:", person1.firstName);
-  console.log("Last Name:", person1.lastName);
+function simbolFn(str,char) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) {
+            count++;
+        }
+    }
+    return count;
+}
+let result = simbolFn(`bibibingo`,`b`)
+console.log(result);
 
-  //TASK5
-  const arr2 = [1, 2, 3, 4, 5];
-console.log( arr2[2])
-
+//TASK5
+function summaFn(arr) {
+  let summa = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 3 === 0) {
+      summa += arr[i];
+    }
+    return summa;
+  }
+  console.log(summa);
+}
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(summaFn(array));
 
 //TASK6
-const person = {};
-
-person.firstName = prompt("Введите имя");
-person.lastName = prompt("Введите фамилию");
-person.age = prompt("Введите возраст");
-
-console.log("Person:", person);
+function dobleChar(str) {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    result += str[i] + str[i];
+  }
+  return result;
+}
+console.log(dobleChar(`akuse`));
 
 //TASK7
-const input = prompt("Введите значение");
-const obj = {};
-const arr = [];
-
-if (input === "Объект") {
-  obj.value = input;
-  console.log("Объект:", obj);
-} else if (input === "Массив") {
-  arr.push(input);
-  console.log("Массив:", arr);
-} else {
-  console.log("error");
+function withZero(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(Math.max(0, arr[i]));
+  }
+  return result;
 }
+console.log(withZero([-1, 3, 4, -5]));
+
+//TASK8
+function isNumber(num) {
+  if (num <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(isNumber(2));
+
+//TASK9
+function fnArray(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(arr[i] * arr[i]);
+  }
+  return result;
+}
+console.log(fnArray([1, 2, 3, 4, 5, 6]));
+
+//TASK10
+
+//TASK11
+function areAnagrams(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  let sortedStr1 = str1.split("").sort().join("");
+  let sortedStr2 = str2.split("").sort().join("");
+  return sortedStr1 === sortedStr2;
+}
+console.log(areAnagrams(`neerg`, `green`));
+
+//TASK12
+function twoElements(arr1, arr2) {
+  let result = [];
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.includes(arr1[i])) {
+      result.push(arr1[i]);
+    }
+  }
+  return result;
+}
+console.log(twoElements([1, 2, 3, 4], [0, 2, 9, 4]));
+
+//TASK13
+function replaceElem(str) {
+  let glasnye = "aeiouAEIOU";
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    if (glasnye.includes(str[i])) {
+      result += "*";
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+}
+console.log(replaceElem(`child`));
+
+//TASK14
+function reverseStrings(arr) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    const str = arr[i];
+    const reversed = str.split("").reverse().join("");
+    result.push(reversed);
+  }
+  return result;
+}
+
+console.log(reverseStrings(["I", "belive", "I", "can"]));
